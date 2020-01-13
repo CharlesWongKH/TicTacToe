336ddfdebf3946ce91ca5d1895e441b64105b7a6
CharlesWong <kwan_ho_wong@cathaypacific.com>
2020-01-14 00:18:09 +0800

    Initial Commit

---
+++ Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Kwan Ho Wong on 14/1/2020.
+//  Copyright © 2020 Kwan Ho Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
